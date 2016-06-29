0b122661915e5949a5afe5c15620ff58f736f098
Madhu <madhu@uncommon.is>
2016-06-29 12:51:53 +0530

    Provide pluggable http client

--- app/src/main/java/uncmn/downlink/sample/MainApplication.java
+++ app/src/main/java/uncmn/downlink/sample/MainApplication.java
@@ -2,6 +2,11 @@ package uncmn.downlink.sample;
 
 import android.app.Application;
 import java.io.File;
+import java.io.IOException;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import timber.log.Timber;
 import uncmn.downlink.Downlink;
 import uncmn.downlink.Logger;
@@ -18,7 +23,15 @@ public class MainApplication extends Application {
     super.onCreate();
     Timber.plant(new TimberThreadDebugTree());
     File downloadDir = new File(getFilesDir(), "downloads");
-    downlink = Downlink.create(downloadDir.getAbsolutePath(), DEFAULT_CACHE_MAX_SIZE_IN_BYTES);
+    OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+      @Override public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        Timber.d("Intercepting request url -- %s", request.url());
+        return chain.proceed(request);
+      }
+    }).build();
+    downlink =
+        Downlink.create(downloadDir.getAbsolutePath(), DEFAULT_CACHE_MAX_SIZE_IN_BYTES, client);
     downlink.setLogger(new Logger() {
       @Override public void log(String message) {
         Timber.d(message);
